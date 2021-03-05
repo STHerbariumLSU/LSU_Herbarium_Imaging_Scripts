d72684a08365bc09cdf147f25e95a6bec84fe810
Herbarium <>
2021-03-05 15:19:13 -0600

    change date march1-2

--- WorkflowScripts/RunWeeklyPortalMap.sh
+++ WorkflowScripts/RunWeeklyPortalMap.sh
@@ -15,8 +15,8 @@ webPath="https://cyberfloralouisiana.com/images/LSUCollections/"
 regular='False'
 nDays=7
 # Year,month,day
-oldDate='2021,2,18'
-newDate='2021,2,24'
+oldDate='2021,3,1'
+newDate='2021,3,2'
 # Path to your python script 
 python3 /mnt/c/Users/Image/Documents/GitHub/LSU_Herbarium_Imaging_Scripts/WorkflowScripts/WeeklyPortalMap.py -l $logFolder -c $csvFolder -w $webPath -r $regular -d $nDays -n $newDate -o $oldDate &>> $elog
 
